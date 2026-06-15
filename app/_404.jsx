43b854be05f18d5c27fa4402318b51f38cdf9b65
Valters Kamaldins <valters@kamaltek.com>
2026-06-16 02:50:46 +0300

    fix

---
+++ app/_404.jsx
@@ -0,0 +1,9 @@
+import { NotFoundPage } from "nextra-theme-docs";
+
+export default function NotFound() {
+  return (
+    <NotFoundPage content="Ziņot par salauztu saiti">
+      404: Lapa nav atrasta
+    </NotFoundPage>
+  );
+}
